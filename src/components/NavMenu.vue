0c8b1c3ee8bf90d42fe175f80aa8252a404a0fa3
BlueNyang <dksrbxo@bluenyang.kr>
2026-01-16 23:17:41 +0900

    feat: add submenu in teams

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -5,6 +5,7 @@ type Item = {
   name: string;
   path: string;
   isBlank: boolean;
+  children?: Item[];
 };
 
 const menuItems = MenuList as Item[];
@@ -14,11 +15,36 @@ const menuItems = MenuList as Item[];
   <ul class="items-center space-x-4 hidden lg:flex font-[Sour_Gummy] text-lg">
     <li v-for="menuItem in menuItems" :key="menuItem.name">
       <a
+        v-if="menuItem.children === undefined"
         :href="menuItem.path"
         :target="menuItem.isBlank ? '_blank' : '_self'"
         class="block px-4 py-5 text-gray-700 dark:text-gray-300 hover:text-sky-600 font-medium dark:hover:text-sky-400 hover:-translate-y-1 transition-all duration-300"
       >
         {{ menuItem.name }}
+      </a>
+      <a
+        v-else
+        href="#"
+        class="block px-4 py-5 text-gray-700 dark:text-gray-300 font-medium relative group"
+      >
+        {{ menuItem.name }}
+        <ul
+          class="absolute -left-2/3 mt-2 w-50 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"
+        >
+          <li
+            v-for="child in menuItem.children"
+            :key="child.name"
+            class="border-b border-gray-200 dark:border-gray-700 last:border-0"
+          >
+            <a
+              :href="child.path"
+              :target="child.isBlank ? '_blank' : '_self'"
+              class="block px-4 py-2 dark:text-gray-300 text-center hover:text-sky-600 font-medium dark:hover:text-sky-400 hover:-translate-y-1 transition-all duration-300"
+            >
+              {{ child.name }}
+            </a>
+          </li>
+        </ul>
       </a>
     </li>
   </ul>
--- src/components/Offcanvas.vue
+++ src/components/Offcanvas.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import MenuList from '@/data/Menu.json';
 import { useSidebar, type SidebarToggle } from '@/stores/useSidebar';
+import { ref } from 'vue';
 
 type Item = {
   name: string;
   path: string;
   isBlank: boolean;
+  children?: Item[];
 };
 
 const { isOpen, toggleSidebar }: SidebarToggle = useSidebar();
+const openedMenu = ref<string | null>(null);
 
 const menuItems = MenuList as Item[];
+
+const toggleMenu = (name: string) => {
+  if (openedMenu.value === name) {
+    openedMenu.value = null;
+  } else {
+    openedMenu.value = name;
+  }
+};
 </script>
 
 <template>
@@ -54,14 +65,56 @@ const menuItems = MenuList as Item[];
       <ul
         class="flex w-full items-center space-y-4 flex-col font-[Sour_Gummy] text-lg"
       >
-        <li v-for="menuItem in menuItems" :key="menuItem.name">
+        <li
+          v-for="menuItem in menuItems"
+          :key="menuItem.name"
+          class="w-full text-center"
+        >
           <a
+            v-if="menuItem.children === undefined"
             :href="menuItem.path"
             :target="menuItem.isBlank ? '_blank' : '_self'"
             class="block px-4 py-5 text-gray-700 dark:text-gray-300 hover:text-sky-600 font-medium dark:hover:text-sky-400 hover:-translate-y-1 transition-all duration-300"
           >
             {{ menuItem.name }}
           </a>
+          <div v-else class="w-full text-center">
+            <button
+              type="button"
+              @click="toggleMenu(menuItem.name)"
+              class="block w-full px-4 py-5 text-gray-700 dark:text-gray-300 font-medium hover:text-sky-600 transition-colors"
+            >
+              {{ menuItem.name }}
+            </button>
+
+            <div
+              class="grid transition-all duration-300 ease-in-out"
+              :class="
+                openedMenu === menuItem.name
+                  ? 'grid-rows-[1fr] opacity-100'
+                  : 'grid-rows-[0fr] opacity-0'
+              "
+            >
+              <ul
+                class="overflow-hidden w-full bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-700 rounded-md shadow-lg z-10"
+                :class="{ border: openedMenu === menuItem.name }"
+              >
+                <li
+                  v-for="child in menuItem.children"
+                  :key="child.name"
+                  class="border-b border-gray-200 dark:border-gray-700 last:border-0"
+                >
+                  <a
+                    :href="child.path"
+                    :target="child.isBlank ? '_blank' : '_self'"
+                    class="block px-4 py-2 dark:text-gray-300 text-center hover:text-sky-600 font-medium dark:hover:text-sky-400 hover:-translate-y-1 transition-all duration-300"
+                  >
+                    {{ child.name }}
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
