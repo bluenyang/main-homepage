8ac54b7d092d9c60e946dad78b178a14622fb3be
BlueNyang <kai@bluenyang.kr>
2026-08-14 00:10:14 +0900

    feat: update packages

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: 'selector',
-  content: ['./index.html', './src/**/*.{vue,js,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
