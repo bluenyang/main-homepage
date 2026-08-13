2d6e09dd0339aae492ff772f73027442a64e37ac
BlueNyang <kai@bluenyang.kr>
2026-08-14 00:07:42 +0900

    feat: use iconify component

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -27,7 +27,7 @@
         >
           <Icon
             icon="mdi:github"
-            class="size-8 text-black transition-all duration-500 hover:-translate-y-1 hover:text-sky-300 dark:text-white dark:hover:text-sky-400"
+            class="size-10 text-black transition-all duration-500 hover:-translate-y-1 hover:text-sky-300 dark:text-white dark:hover:text-sky-400"
           />
         </a>
         <a
@@ -36,19 +36,10 @@
           rel="noreferrer"
           class="flex h-fit items-center justify-center"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            class="bi bi-instagram fill-black transition-all duration-400 hover:-translate-y-1 hover:fill-sky-300 dark:fill-white dark:hover:fill-sky-400"
-            viewBox="0 0 16 16"
-          >
-            <title>인스타그램 로고</title>
-            <path
-              d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"
-            />
-          </svg>
+          <Icon
+            icon="mdi:instagram"
+            class="size-10 text-black transition-all duration-500 hover:-translate-y-1 hover:text-sky-300 dark:text-white dark:hover:text-sky-400"
+          />
         </a>
         <a
           href="https://bluenyang-dev.tistory.com/"
@@ -56,61 +47,32 @@
           rel="noreferrer"
           class="flex h-fit items-center justify-center"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 459 459"
-            fill="currentColor"
-            width="32"
-            height="32"
-            class="fill-black transition-all duration-400 hover:-translate-y-1 hover:fill-sky-300 dark:fill-white dark:hover:fill-sky-400"
-          >
-            <title>티스토리 로고</title>
-            <g>
-              <path
-                d="M229.5,0C102.75,0,0,102.75,0,229.5S102.75,459,229.5,459,459,356.25,459,229.5,356.25,0,229.5,0ZM130.21,191.45a39.57,39.57,0,1,1,39.56-39.57A39.58,39.58,0,0,1,130.21,191.45ZM229.5,390a39.56,39.56,0,1,1,39.56-39.56A39.56,39.56,0,0,1,229.5,390Zm0-99.29a39.56,39.56,0,1,1,39.56-39.56A39.56,39.56,0,0,1,229.5,290.74Zm0-99.29a39.57,39.57,0,1,1,39.56-39.57A39.57,39.57,0,0,1,229.5,191.45Zm99.29,0a39.57,39.57,0,1,1,39.57-39.57A39.57,39.57,0,0,1,328.79,191.45Z"
-              />
-            </g>
-          </svg>
+          <Icon
+            icon="lucide:coffee"
+            class="size-10 text-black transition-all duration-500 hover:-translate-y-1 hover:text-sky-300 dark:text-white dark:hover:text-sky-400"
+          />
         </a>
         <a
-          href="https://ocis.croffledev.kr/s/hTHvjhkcEGwNevm"
+          href="https://dl.bluenyang.kr/public/"
           target="_blank"
           rel="noreferrer"
           class="flex h-fit items-center justify-center"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="currentColor"
-            class="bi bi-cloud-arrow-down-fill fill-black transition-all duration-400 hover:-translate-y-1 hover:fill-sky-300 dark:fill-white dark:hover:fill-sky-400"
-            viewBox="0 0 16 16"
-          >
-            <title>파일시스템 로고</title>
-            <path
-              d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2m2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708"
-            />
-          </svg>
+          <Icon
+            icon="mdi:download"
+            class="size-10 text-black transition-all duration-500 hover:-translate-y-1 hover:text-sky-300 dark:text-white dark:hover:text-sky-400"
+          />
         </a>
         <a
-          href="https://docs.bluenyang.kr/s/public"
+          href="https://docs.bluenyang.kr/s/public/"
           target="_blank"
           rel="noreferrer"
           class="flex h-fit items-center justify-center"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 56 56"
-            class="fill-black transition-all duration-400 hover:-translate-y-1 hover:fill-sky-300 dark:fill-white dark:hover:fill-sky-400"
-            fill="currentColor"
-          >
-            <title>공개 문서 로고</title>
-            <path
-              d="M15.555 53.125h24.89c4.852 0 7.266-2.461 7.266-7.336V24.508H30.742c-3 0-4.406-1.43-4.406-4.43V2.875H15.555c-4.828 0-7.266 2.484-7.266 7.36v35.554c0 4.898 2.438 7.336 7.266 7.336m15.258-31.828h16.64c-.164-.961-.844-1.899-1.945-3.047L32.57 5.102c-1.078-1.125-2.062-1.805-3.047-1.97v16.9c0 .843.446 1.265 1.29 1.265"
-            />
-          </svg>
+          <Icon
+            icon="mdi:file-document-outline"
+            class="size-10 text-black transition-all duration-500 hover:-translate-y-1 hover:text-sky-300 dark:text-white dark:hover:text-sky-400"
+          />
         </a>
       </div>
     </div>
